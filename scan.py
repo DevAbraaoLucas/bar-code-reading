3ede792b3be88094dc4a2a960261635fb338e672
Abraão Lucas <luckzinrr@gmail.com>
2026-07-10 17:09:19 -0300

    implementing PO and invoice number substitutions
    In this version, I added a condition to check if the invoice number or PO number was actually found; if not, they are set to "undefined" so the code doesn't throw an error and disrupt the processing of subsequent invoices.

--- scan.py
+++ scan.py
@@ -9,15 +9,24 @@ pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 with open(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf") as doc: # abre o pdf
     page = doc[0] # pega a primeira página do pdf
-    img_dpi = Matrix(6, 6) # aumenta a resolução da imagem
+    img_dpi = Matrix(6.7, 6.7) # aumenta a resolução da imagem
     img_pixels = doc[0].get_pixmap(matrix=img_dpi) # converte a primeira página do pdf em imagem
     img = Image.frombytes("RGB", [img_pixels.width, img_pixels.height], img_pixels.samples) # converte a imagem em um objeto PIL
-
-    leitura_cod_barras = decode(img) # decodifica o código de barras da imagem
-    codigo_barras = leitura_cod_barras[0].data.decode("utf-8") # pega o código de barras decodificado e transforma em string
-    numero_NF = codigo_barras[29:34] # pega o número da nota fiscal do código de barras
-
+    
     txt = image_to_string(img, lang='por') # lê o texto da imagem e transforma em string
+    
+    leitura_cod_barras = decode(img) # decodifica o código de barras da imagem
+    
+    if leitura_cod_barras:
+        codigo_barras = leitura_cod_barras[0].data.decode("utf-8") # pega o código de barras decodificado e transforma em string
+        numero_NF = codigo_barras[29:34] # pega o número da nota fiscal do código de barras
+    else:
+        procurar_NF = search(r"FATURA[\s\S]{0,150}?(\d{4,6})", txt) # procura o número da nota fiscal no texto da imagem caso tenha algum erro na leitura do código de barras
+        if procurar_NF:
+            numero_NF = procurar_NF.group(1) # pega o número da nota fiscal encontrado
+        else:
+            numero_NF = "undefined" # define o número da nota fiscal como "não encontrada" se não for encontrado
+
     procurar_PO = search(r"PO[\s]*(\d+)", txt) # procura o número do pedido de compra no texto da imagem
     if procurar_PO:
         if "PO" in txt: # verifica se o texto contém a palavra "PO"
@@ -26,8 +35,8 @@ with open(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf") as doc
                 numero_PO = f"{numero_PO} PARCIAL" # adiciona a palavra "PARCIAL" ao número da PO
             else:
                 numero_PO = f"{numero_PO}" # define o número da PO como está
-        else:
-            numero_PO = "não encontrada" # define o número da PO como "PO não encontrada" se não for encontrada
+    else:
+        numero_PO = "undefined" # define o número da PO como "PO não encontrada" se não for encontrada
 
 Path(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf").rename( # renomeia o arquivo pdf com o número da NF e da PO
     rf"c:\Users\Murilo\OneDrive\Documents\Digitalizados\NF {numero_NF} PO {numero_PO}.pdf"
