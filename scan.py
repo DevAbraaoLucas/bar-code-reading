9dc6419a73a5936d03a69125a3571d03e08b325b
Abraão Lucas <luckzinrr@gmail.com>
2026-07-24 09:14:48 -0300

    updates
    Now my script can read the full invoice number and strip all leading zeros, resulting in a cleaner number when renaming.

--- scan.py
+++ scan.py
@@ -19,7 +19,7 @@ with open(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf") as doc
     
     if leitura_cod_barras:
         codigo_barras = leitura_cod_barras[0].data.decode("utf-8") # pega o código de barras decodificado e transforma em string
-        numero_NF = codigo_barras[29:34] # pega o número da nota fiscal do código de barras
+        numero_NF = codigo_barras[25:34] # pega o número da nota fiscal do código de barras
     else:
         procurar_NF = search(r"FATURA[\s\S]{0,150}?(\d{4,6})", txt) # procura o número da nota fiscal no texto da imagem caso tenha algum erro na leitura do código de barras
         if procurar_NF:
@@ -27,17 +27,20 @@ with open(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf") as doc
         else:
             numero_NF = "undefined" # define o número da nota fiscal como "não encontrada" se não for encontrado
 
+    while numero_NF[0] == '0':
+        numero_NF = numero_NF[1:]
+    
     procurar_PO = search(r"PO[\s]*(\d+)", txt) # procura o número do pedido de compra no texto da imagem
     if procurar_PO:
         if "PO" in txt: # verifica se o texto contém a palavra "PO"
             numero_PO = procurar_PO.group(1) # pega o número do pedido de compra encontrado
             if "PARCIAL" in txt: # verifica se a nota fiscal é parcial
-                numero_PO = f"{numero_PO} PARCIAL" # adiciona a palavra "PARCIAL" ao número da PO
+                numero_PO = f"PO {numero_PO} PARCIAL" # adiciona a palavra "PARCIAL" ao número da PO
             else:
-                numero_PO = f"{numero_PO}" # define o número da PO como está
+                numero_PO = f"PO {numero_PO}" # define o número da PO como está
     else:
         numero_PO = "undefined" # define o número da PO como "PO não encontrada" se não for encontrada
 
 Path(r"c:\Users\Murilo\OneDrive\Documents\Digitalizados\doc001.pdf").rename( # renomeia o arquivo pdf com o número da NF e da PO
-    rf"c:\Users\Murilo\OneDrive\Documents\Digitalizados\NF {numero_NF} PO {numero_PO}.pdf"
+    rf"c:\Users\Murilo\OneDrive\Documents\Digitalizados\NF {numero_NF} {numero_PO}.pdf"
 )
